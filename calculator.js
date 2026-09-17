e15cf62dda64c749e58a3e64f33514d46ec5c9ee
kayrichie05 <kscobb2@aggies.ncat.edu>
2026-09-17 17:56:19 -0400

    String output

---
+++ calculator.js
@@ -0,0 +1,10 @@
+let tableRows = "";
+let validResults = [];
+
+document.write("<style>");
+document.write("table { border-collapse: collapse; margin-bottom: 20px; font-family: sans-serif; }");
+document.write("th, td { border: 1px solid black; padding: 8px 15px; text-align: center; }");
+document.write("th { background-color: orange; font-weight: bold; }");
+document.write("td { background-color: #fdf6e3; }");
+document.write("</style>");
+
